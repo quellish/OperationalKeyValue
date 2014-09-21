cc95be344680589d37d356d3698c0eaff2dbaead
robertmryan <robert.ryan@mindspring.com>
2014-09-21 00:16:45 -0400

    Make operation truly concurrent.
    This original operation might have returned `YES` for `isConcurrent`, but by the time `main` finished, you had already set the `finished` value. In that situation, it behaves like a non-concurrent operation (i.e. just checks `isFinished` getter when `main` is done, and if it returns `YES`, it terminates operation immediately, not worrying about KVN). But if you make this truly behave like a concurrent operation (i.e. return immediately, do additional stuff asynchronously, and set `finished` at a later point), you'll see why the custom `isFinished` KVN notification is critical.
    
    As an aside, Apple generally puts the `isCancelled` check in `start`, so I put it there, but that's not really material to the real problem, that you didn't have this behave like a true concurrent/asynchronous operation.
    
    I also changed the `finished`/`executing` properties to be `nonatomic`, so I could define custom setters.

--- OperationalKeyValue/SampleOperation.m
+++ OperationalKeyValue/SampleOperation.m
@@ -8,33 +8,62 @@
 
 #import "SampleOperation.h"
 
+static NSString * const kFinishedKey = @"isFinished";
+static NSString * const kExecutingKey = @"isExecuting";
+
 @interface SampleOperation ()
-@property (atomic, readwrite, getter=isExecuting) BOOL executing;
-@property (atomic, readwrite, getter=isFinished)  BOOL finished;
+@property (nonatomic, readwrite, getter=isExecuting) BOOL executing;
+@property (nonatomic, readwrite, getter=isFinished)  BOOL finished;
 @end
 
 @implementation SampleOperation
-@synthesize executing;
-@synthesize finished;
+@synthesize executing = _executing;
+@synthesize finished  = _finished;
 
 - (void) main {
-    if (![self isCancelled]){
-        [self setExecuting:YES];
-        [self setFinished:NO];
-        
-        // Something would happen here.
-        
-        [self setFinished:YES];
+
+    NSLog(@"starting operation");
+
+    // do something asynchronously
+
+    // e.g., we're going to let `main` return, but two seconds later, we'll end this operation
+
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2.0 * NSEC_PER_SEC), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSLog(@"finishing operation");
+
         [self setExecuting:NO];
-    }
+        [self setFinished:YES];
+    });
 }
 
 - (void) start {
+
+    if ([self isCancelled]) {
+        [self setFinished:YES];
+        return;
+    }
+
+    [self setExecuting:YES];
+
     [self main];
 }
 
 - (BOOL) isConcurrent {
     return YES;
+}
+
+- (void)setExecuting:(BOOL)executing
+{
+    [self willChangeValueForKey:kExecutingKey];
+    _executing = executing;
+    [self didChangeValueForKey:kExecutingKey];
+}
+
+- (void)setFinished:(BOOL)finished
+{
+    [self willChangeValueForKey:kFinishedKey];
+    _finished = finished;
+    [self didChangeValueForKey:kFinishedKey];
 }
 
 /***
